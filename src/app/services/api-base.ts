9b0b8f3e4d7451e0d6a800df8566bb2241e7c2fc
mhelwa <mhelwa@2p.com.sa>
2026-05-20 10:52:25 +0300

    update: remove footer

--- src/app/services/api-base.ts
+++ src/app/services/api-base.ts
@@ -1,5 +1,4 @@
-export const API_BASE = 'https://937.moh.gov.sa/haj-surveys-api/api';
-
+export const API_BASE = 'https://px937-api.moh.gov.sa/api';
 // TODO: replace with a proper auth flow once login is wired up.
 export const AUTH_TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJsaXNhIiwibmJmIjoxNzM5MDI4ODE2LCJleHAiOjE3Mzk2MzM2MTYsImlhdCI6MTczOTAyODgxNn0.sLQamqv3NCvDsEe4wfdjXzPlXC4QxrLYXJzTPcrjpeE';
